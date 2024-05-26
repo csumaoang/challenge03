fde088729207b2341909e15d24f42f3cd6162ea4
csumaoang <csumaoang@academic.rrc.ca>
2024-05-26 17:17:23 -0500

    Figured out info about Product class using challenge_read.rb

--- challenge_read.rb
+++ challenge_read.rb
@@ -1,5 +1,16 @@
 require_relative 'ar.rb'
 
+# Total number of products
 number_of_products = Product.count
-
 puts "There are #{number_of_products} in the products table."
+
+# The names of all products above $10 with names that begin with the letter C.
+products_above_10_with_c_names = Product.where('price > 10 AND name LIKE "C%"')
+puts "Products above $10 with names that begin with the letter C:"
+products_above_10_with_c_names.each do |product|
+  puts "#{product.name}"
+end
+
+# Total number of products with a low stock quantity. (Low is defined as less than 5 in stock.)
+products_low_stock = Product.where('stock_quantity < 5').count
+puts "Total number of products with low stock quantity: #{products_low_stock}"
--- models/product.rb
+++ models/product.rb
@@ -5,4 +5,7 @@ class Product < ActiveRecord::Base
   # The products table has a category_id foreign key.
   # In other words, a product belongs to a category.
   belongs_to :category
+
+  # Columns in the products table are: id, name, description, price, stock_quantity, category_id, created_at, and updated_at
+  # Based on the columns found, the category table has an association with the products table because of category_id
 end
